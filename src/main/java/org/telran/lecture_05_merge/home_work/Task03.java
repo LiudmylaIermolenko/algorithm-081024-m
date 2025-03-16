d5cbb0a9ad41b8cad2bd6822b3d59da21261c564
LiudmylaIermolenko <lussi.yer@gmail.com>
2025-03-16 14:02:22 +0100

    H_W_16_03_2025

--- src/main/java/org/telran/lecture_05_merge/home_work/Task03.java
+++ src/main/java/org/telran/lecture_05_merge/home_work/Task03.java
@@ -24,23 +24,63 @@ import static org.telran.lecture_05_merge.practice.MergeSort.mergeSort;
 
 public class Task03 {
     public static void main(String[] args) {
-        int[] prices = {10, 2, 50, 1, 10, 30, 7}; // Цены товаров которые вы купили
+        int[] prices = {10, 2, 50, 1, 10, 30, 7};
         int[] sortedPrices = mergeSort(prices);
         System.out.println(Arrays.toString(sortedPrices));
 
         int middle = sortedPrices.length / 2;
+        int sum = 0;
+        for (int i = middle; i < sortedPrices.length; i++) {
+        sum += sortedPrices[i];
+        }
         //Arrays.copyOfRange(originalArray, from, to);
         int[] expensiveItems = Arrays.copyOfRange(sortedPrices, middle, sortedPrices.length);
         System.out.println(Arrays.toString(expensiveItems));
 
-        int sum = 0;
-        for (int price : expensiveItems) {
-            sum += price;
-        }
+//        int sum = 0;
+//        for (int price : expensiveItems) {
+//            sum += price;
+//        }
 
         System.out.println("Максимальная сумма чека: " + sum);
     }
 }
-
+//  // Рекурсивно вызываем mergeSort() для левой и правой частей.
+//Объединяем отсортированные части в один массив с помощью merge().
+//    public static int[] mergeSort(int[] array) {
+//        if (array.length <= 1) {
+//            return array;
+//        }
+//
+//        int middle = array.length / 2;
+//        int[] left = Arrays.copyOfRange(array, 0, middle);
+//        int[] right = Arrays.copyOfRange(array, middle, array.length);
+//
+//        return merge(mergeSort(left), mergeSort(right));
+//    }
+//
+//    public static int[] merge(int[] left, int[] right) {
+//        int[] result = new int[left.length + right.length];
+//        int i = 0, j = 0, k = 0;
+//
+//        while (i < left.length && j < right.length) {
+//            if (left[i] <= right[j]) {
+//                result[k++] = left[i++];
+//            } else {
+//                result[k++] = right[j++];
+//            }
+//        }
+//
+//        while (i < left.length) {
+//            result[k++] = left[i++];
+//        }
+//
+//        while (j < right.length) {
+//            result[k++] = right[j++];
+//        }
+//
+//        return result;
+//    }
+//}
 
 
